d052be4d11ff0ca5536399a2d25d2f501622191c
Wilson Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2024-02-16 17:24:27 +0530

    MainScreen Bottom Nav

--- lib/MainScreen.dart
+++ lib/MainScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:iwayplusnav/BuildingSelectionScreen.dart';
 import 'package:iwayplusnav/Navigation.dart';
 
@@ -18,7 +19,6 @@ class _MainScreenState extends State<MainScreen> {
   final screens = [
     BuildingSelectionScreen(),
     Navigation(buildingID: '',),
-    Navigation(buildingID: ''),
     BuildingSelectionScreen(),
     BuildingSelectionScreen(),
   ];
@@ -50,10 +50,18 @@ class _MainScreenState extends State<MainScreen> {
           backgroundColor: Color(0xffFFFFFF),
           selectedIndex: index,
           onDestinationSelected: (index)=>setState(() {
-
+            if (index == 3 || index==2 || index==4 || index==1 ){
+              // Check if the 4th screen is selected
+              showToast('Feature coming soon');
+            } else {
+              // Switch to the selected screen for other cases
+              setState(() {
+                this.index = index;
+              });
+            }
           }),
           destinations: [
-            NavigationDestination(icon: SvgPicture.asset("assets/MainScreen_home.svg",color: Color(0xff1C1B1F)),selectedIcon: SvgPicture.asset("assets/MainScreen_home.svg",color: Color(0xff24B9B0),), label: '',),
+            NavigationDestination(icon: SvgPicture.asset("assets/MainScreen_home.svg",color: Color(0xff1C1B1F)),selectedIcon: SvgPicture.asset("assets/MainScreen_home.svg",color: Color(0xff24B9B0),), label: 'Home',),
             NavigationDestination(icon: SvgPicture.asset("assets/MainScreen_Map.svg",color: Color(0xff1C1B1F)),selectedIcon: SvgPicture.asset("assets/MainScreen_Map.svg",color: Color(0xff24B9B0),), label: "Map",),
             NavigationDestination(icon: SvgPicture.asset("assets/MainScreen_Scanner.svg",color: Color(0xff1C1B1F),),selectedIcon: SvgPicture.asset("assets/MainScreen_Scanner.svg",color: Color(0xff1C1B1F),width: 34,height: 34,), label: 'Scan',),
             NavigationDestination(icon: SvgPicture.asset("assets/MainScreen_Favourite.svg",color: Color(0xff1C1B1F),),selectedIcon: SvgPicture.asset("assets/MainScreen_Favourite.svg",color: Color(0xff1C1B1F),), label: "Favourite",),
@@ -61,6 +69,18 @@ class _MainScreenState extends State<MainScreen> {
           ],
         ),
       ),
+    );
+  }
+
+  void showToast(String mssg) {
+    Fluttertoast.showToast(
+      msg: mssg,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.grey,
+      textColor: Colors.white,
+      fontSize: 16.0,
     );
   }
 }

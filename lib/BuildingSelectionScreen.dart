97db9145155e9bbb7a4f434603e3c589c926ab77
Wilson Daniel <77532147+Wilson-Daniel@users.noreply.github.com>
2024-02-17 16:49:56 +0530

    Dashboard

--- lib/BuildingSelectionScreen.dart
+++ lib/BuildingSelectionScreen.dart
@@ -1,4 +1,9 @@
 
+import 'dart:collection';
+
+import 'package:anim_search_bar/anim_search_bar.dart';
+import 'package:easy_search_bar/easy_search_bar.dart';
+import 'package:expandable_search_bar/expandable_search_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
@@ -8,6 +13,7 @@ import 'package:iwayplusnav/Elements/buildingCard.dart';
 
 import 'API/buildingAllApi.dart';
 import 'APIMODELS/buildingAllModel.dart';
+import 'Elements/HomeNestedSearch.dart';
 import 'Navigation.dart';
 
 class BuildingSelectionScreen extends StatefulWidget{
@@ -17,8 +23,11 @@ class BuildingSelectionScreen extends StatefulWidget{
 
 class _BuildingSelectionScreenState extends State<BuildingSelectionScreen>{
   late List<buildingAllModel> buildingList=[];
+  late List<buildingAllModel> newbuildingList=[];
   bool isLoading_buildingList = true;
   List<Widget> BuildingCard = [];
+
+
   @override
   void initState(){
     super.initState();
@@ -30,6 +39,7 @@ class _BuildingSelectionScreenState extends State<BuildingSelectionScreen>{
     await buildingAllApi().fetchBuildingAllData().then((value) {
       setState(() {
         buildingList = value;
+        newbuildingList = value;
         createBuildingCards(buildingList);
         isLoading_buildingList = false; // Set loading to false when data is loaded
       });
@@ -38,6 +48,7 @@ class _BuildingSelectionScreenState extends State<BuildingSelectionScreen>{
 
   void createBuildingCards(List<buildingAllModel> buildingList){
     setState(() {
+
       BuildingCard.add(SizedBox(height: 12,));
       for(int i = 0; i<buildingList.length; i++){
         BuildingCard.add(buildingCard(imageURL: buildingList[i].photo != null? buildingList[i].photo!:"", Name: buildingList[i].buildingName!, Tag: buildingList[i].category != null?buildingList[i].category!:"", Address: buildingList[i].address!, Distance: 119, NumberofBuildings: 3, bid: buildingList[i].sId!,));
@@ -46,8 +57,14 @@ class _BuildingSelectionScreenState extends State<BuildingSelectionScreen>{
   }
 
 
+
+
+
+
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -59,7 +76,7 @@ class _BuildingSelectionScreenState extends State<BuildingSelectionScreen>{
                 fontFamily: "Roboto",
                 fontSize: 24,
                 fontWeight: FontWeight.w700,
-                color: Color(0xff000000),
+                color: Color(0xffFFFFFF),
               ),
             ),
           ),
@@ -79,49 +96,198 @@ class _BuildingSelectionScreenState extends State<BuildingSelectionScreen>{
                 ),
               ),
               child: IconButton(
+                icon: Icon(Icons.search),
+                color: Color(0xffFFFFFF),
                 onPressed: () {
-                  // Handle search icon pressed
-                  // You can add your search functionality here
+                  showSearch(context: context, delegate: HomeNestedSearch(newbuildingList));
                 },
-                icon: Icon(
-                  Icons.search,
-                  color: Colors.black,
-                ),
-                iconSize: 30,
+              ))
+          ],
+          backgroundColor: Colors.transparent, // Set the background color to transparent
+          elevation: 0,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Color(0xFF0F6862), Color(0xFF032137)], // Set your gradient colors
               ),
             ),
-          ],
-          backgroundColor: Color(0xffFFFFFF),
-          elevation: 0,
+          ),
         ),
         body: isLoading_buildingList
-            ? Center(
+                ? Center(
                 child: Animate(
-                  effects: [FadeEffect(), ScaleEffect()],
-                  child: Text("Loading Data!!",style: TextStyle(
-                        fontSize: 30,
-                        color: Color(0xFF666870),
-                        height: 1,
-                        letterSpacing: -1,
-                      ),)
-                      .animate(onPlay: (controller) => controller.repeat())
-                      .shimmer(duration: 1200.ms, color: const Color(0xFF80DDFF))
-                      .animate() // this wraps the previous Animate in another Animate
-                      .fade(duration: 1200.ms, curve: Curves.ease)
-                      .slide()
+                    effects: [FadeEffect(), ScaleEffect()],
+                    child: Text("Loading Data!!",style: TextStyle(
+                      fontSize: 30,
+                      color: Color(0xFF666870),
+                      height: 1,
+                      letterSpacing: -1,
+                    ),)
+                        .animate(onPlay: (controller) => controller.repeat())
+                        .shimmer(duration: 1200.ms, color: const Color(0xFF80DDFF))
+                        .animate() // this wraps the previous Animate in another Animate
+                        .fade(duration: 1200.ms, curve: Curves.ease)
+                        .slide()
                 )
-                // Show linear loading indicator
-              )
-              : SingleChildScrollView(
-                child: Column(
-          children: BuildingCard,
-        ),
-              )
+              // Show linear loading indicator
+            )
+            : DefaultTabController(
+            length: 5,
+            child: Column(
+              children: [
+                Material(
+                  child: Container(
+                    height: 55,
+                    color: Color(0xffFFFFFF),
+                    child: Container(
+                      height: 55,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [Color(0xFF0F6862), Color(0xFF032137)], // Set your gradient colors
+                        ),
+                      ),
+                      child: TabBar(
+                        unselectedLabelColor: Color(0xffB3B3B3),
+                        isScrollable: true,
+                        indicatorColor: Colors.white,
+                        labelColor: Colors.white,
+
+                        tabs: [
+                          Tab(child: Container(
+                              height: 35,
+                              child: Align(
+                                alignment: Alignment.center,
+                                child: Text("All"),
+                              ),
+                            ),
+                          ),
+                          Tab(child: Container(
+                            height: 35,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text("Academic"),
+                            ),
+                          ),
+                          ),
+                          Tab(child: Container(
+                            height: 35,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text("Hospital"),
+                            ),
+                          ),
+                          ),
+                          Tab(child: Container(
+                            height: 35,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text("Mall"),
+                            ),
+                          ),
+                          ),
+                          Tab(child: Container(
+                            height: 35,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text("Event"),
+                            ),
+                          ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                    child: TabBarView(
+                      children: [
+                        ListView.builder(
+                            itemBuilder: (context,index){
+                              var currentData = newbuildingList[index];
+                              return buildingCard(imageURL: currentData.photo??"",
+                                Name: currentData.buildingName??"",
+                                Tag: currentData.category?? "", Address: currentData.address?? "", Distance: 190, NumberofBuildings: 3, bid: currentData.sId??"",);
+                            },
+                            itemCount: newbuildingList.length
+                        ),
+                        ListView.builder(
+                          itemBuilder: (context, index) {
+                            var currentData = newbuildingList[index];
+                            if (currentData.category == "Academic") {
+                              return buildingCard(
+                                imageURL: currentData.photo ?? "",
+                                Name: currentData.buildingName ?? "",
+                                Tag: currentData.category ?? "",
+                                Address: currentData.address ?? "",
+                                Distance: 190,
+                                NumberofBuildings: 3,
+                                bid: currentData.sId ?? "",
+                              );
+                            } else {
+                              return SizedBox.shrink(); // Empty widget if not Hospital
+                            }
+                          },
+                          itemCount: newbuildingList.length,
+                        ),
+                        ListView.builder(
+                          itemBuilder: (context, index) {
+                            var currentData = newbuildingList[index];
+                            if (currentData.category == "Hospital") {
+                              return buildingCard(
+                                imageURL: currentData.photo ?? "",
+                                Name: currentData.buildingName ?? "",
+                                Tag: currentData.category ?? "",
+                                Address: currentData.address ?? "",
+                                Distance: 190,
+                                NumberofBuildings: 3,
+                                bid: currentData.sId ?? "",
+                              );
+                            } else {
+                              return SizedBox.shrink(); // Empty widget if not Hospital
+                            }
+                          },
+                          itemCount: newbuildingList.length,
+                        ),
+
+                      ],
+
+                    )
+                )
+              ],
+            )
+        )
+        // AnimationLimiter(
+        //   child: ListView.builder(
+        //     itemCount: newbuildingList.length,
+        //     itemBuilder: (context, index) {
+        //       // Apply animation to each ListTile
+        //       return AnimationConfiguration.staggeredList(
+        //         position: index,
+        //         duration: const Duration(milliseconds: 500),
+        //         child: SlideAnimation(
+        //           verticalOffset: 50.0,
+        //           child: FadeInAnimation(
+        //             child: buildingCard(imageURL: newbuildingList[index].photo??"",
+        //               Name: newbuildingList[index].buildingName??"",
+        //               Tag: newbuildingList[index].category?? "", Address: newbuildingList[index].address?? "", Distance: 190, NumberofBuildings: 3, bid: newbuildingList[index].sId??"",)
+        //           ),
+        //
+        //         ),
+        //
+        //       );
+        //     },
+        //
+        //   ),
+        // )
+    ,
 
       ),
     );
   }
-
 
 
 }
---
+++ lib/Elements/HomeNestedSearch.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import '../APIMODELS/buildingAllModel.dart';
+import 'buildingCard.dart';
+
+class HomeNestedSearch extends SearchDelegate{
+  List<buildingAllModel> searchList=[];
+
+  HomeNestedSearch(List<buildingAllModel> initialSearchList) {
+    searchList = initialSearchList;
+  }
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: (){
+            query = '';
+          },
+          icon: const Icon(Icons.clear)
+      )
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: (){
+          close(context, null);
+        },
+        icon: const Icon(Icons.clear)
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<buildingAllModel> bbsearchList = [];
+    for(var item in searchList){
+      if(item.buildingName!.toLowerCase().contains(query.toLowerCase())){
+        bbsearchList.add(item);
+      }
+    }
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        var data = bbsearchList[index];
+        return buildingCard(imageURL: data.photo??"",
+          Name: data.buildingName??"",
+          Tag: data.category?? "", Address: data.address?? "", Distance: 190, NumberofBuildings: 3, bid: data.sId??"",);
+      },
+
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<buildingAllModel> bbsearchList = [];
+    for (var item in searchList) {
+      if (item.buildingName!.toLowerCase().contains(query.toLowerCase()) || item.venueName!.toLowerCase().contains(query.toLowerCase())) {
+        bbsearchList.add(item);
+      }
+    }
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        var data = bbsearchList[index];
+        return buildingCard(imageURL: data.photo ?? "",
+          Name: data.buildingName ?? "",
+          Tag: data.category ?? "",
+          Address: data.address ?? "",
+          Distance: 190,
+          NumberofBuildings: 3,
+          bid: data.sId ?? "",);
+      },
+      itemCount: bbsearchList.length,
+
+    );
+  }
+}

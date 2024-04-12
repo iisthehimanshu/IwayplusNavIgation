cec6f152af8648376347c63e39782eab85e99f03
Udit Soni <91983775+udit6023@users.noreply.github.com>
2024-04-12 15:02:16 +0530

    multi floor path data

--- lib/Elements/directionInstruction.dart
+++ lib/Elements/directionInstruction.dart
@@ -14,7 +14,7 @@ class directionInstruction extends StatefulWidget {
 
 class _directionInstructionState extends State<directionInstruction> {
   Icon icon = Icon(Icons.directions);
-  String distance  = "";
+  String distance = "";
   @override
   void initState() {
     super.initState();
@@ -24,55 +24,55 @@ class _directionInstructionState extends State<directionInstruction> {
   }
 
   Icon getCustomIcon(String direction) {
-    if(direction == "Go Straight"){
+    if (direction == "Go Straight") {
       return Icon(
         Icons.straight,
         color: Colors.black,
         size: 32,
       );
-    }else if(direction == "Turn Slight Right, and Go Straight"){
+    } else if (direction == "Turn Slight Right, and Go Straight") {
       return Icon(
         Icons.turn_slight_right,
         color: Colors.black,
         size: 32,
       );
-    }else if(direction == "Turn Right, and Go Straight"){
+    } else if (direction == "Turn Right, and Go Straight") {
       return Icon(
         Icons.turn_right,
         color: Colors.black,
         size: 32,
       );
-    }else if(direction == "Turn Sharp Right, and Go Straight"){
+    } else if (direction == "Turn Sharp Right, and Go Straight") {
       return Icon(
         Icons.turn_sharp_right,
         color: Colors.black,
         size: 32,
       );
-    }else if(direction == "Turn U Turn, and Go Straight"){
+    } else if (direction == "Turn U Turn, and Go Straight") {
       return Icon(
         Icons.u_turn_right,
         color: Colors.black,
         size: 32,
       );
-    }else if(direction == "Turn Sharp Left, and Go Straight"){
+    } else if (direction == "Turn Sharp Left, and Go Straight") {
       return Icon(
         Icons.turn_sharp_left,
         color: Colors.black,
         size: 32,
       );
-    }else if(direction == "Turn Left, and Go Straight"){
+    } else if (direction == "Turn Left, and Go Straight") {
       return Icon(
         Icons.turn_left,
         color: Colors.black,
         size: 32,
       );
-    }else if(direction == "Turn Slight Left, and Go Straight"){
+    } else if (direction == "Turn Slight Left, and Go Straight") {
       return Icon(
         Icons.turn_slight_left,
         color: Colors.black,
         size: 32,
       );
-    }else{
+    } else {
       return Icon(
         Icons.check_box_outline_blank,
         color: Colors.black,
@@ -85,16 +85,17 @@ class _directionInstructionState extends State<directionInstruction> {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
+    print("direction lift: $widget.direction");
     return Column(
       children: [
         Container(
           height: 45,
           margin: EdgeInsets.only(top: 8),
-          child:Row(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Semantics(
-                label:widget.direction+"${widget.distance} m",
+                label: widget.direction + "${widget.distance} m",
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -106,7 +107,7 @@ class _directionInstructionState extends State<directionInstruction> {
                           fontSize: 16,
                           fontWeight: FontWeight.w400,
                           color: Color(0xff0e0d0d),
-                          height: 25/16,
+                          height: 25 / 16,
                         ),
                         textAlign: TextAlign.left,
                       ),
@@ -119,7 +120,7 @@ class _directionInstructionState extends State<directionInstruction> {
                           fontSize: 14,
                           fontWeight: FontWeight.w400,
                           color: Color(0xff8d8c8c),
-                          height: 20/14,
+                          height: 20 / 14,
                         ),
                         textAlign: TextAlign.left,
                       ),
@@ -129,18 +130,19 @@ class _directionInstructionState extends State<directionInstruction> {
               ),
               Spacer(),
               Container(
-                height: 40,
-                width: 40,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(color: Colors.black),
-                ),
-                child: icon
-              )
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.black),
+                  ),
+                  child: icon)
             ],
           ),
         ),
-        SizedBox(height: 9,),
+        SizedBox(
+          height: 9,
+        ),
         Container(
           width: screenWidth,
           height: 1,

de38b8b910397d96cbf0bde1b1aafb2974815e2a
Udit Soni <uditsoni6023@gmail.com>
2024-10-24 16:52:04 +0530

    changes

--- lib/Elements/AccessiblePathButton.dart
+++ lib/Elements/AccessiblePathButton.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 import '../pathState.dart';
 import '../singletonClass.dart';
 
-class AccessiblePathButton extends StatelessWidget {
+class AccessiblePathButton extends StatefulWidget {
   final String label;
   final IconData icon;
   final String accessibleBy;
   final Function calculateroute;
-  pathState PathState;
+  final pathState PathState;
 
   AccessiblePathButton({
     required this.label,
@@ -19,55 +19,89 @@ class AccessiblePathButton extends StatelessWidget {
   });
 
   @override
+  _AccessiblePathButtonState createState() => _AccessiblePathButtonState();
+}
+
+class _AccessiblePathButtonState extends State<AccessiblePathButton> {
+  bool isHovered = false;
+
+  @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: (){
-        PathState.accessiblePath = accessibleBy;
-        PathState.clearforaccessiblepath();
-        SingletonFunctionController
-            .building.landmarkdata!
-            .then((value) async {
-          try {
-            await Future.delayed(Duration(milliseconds: 10));
-            calculateroute(value.landmarksMap!,
-                accessibleby: accessibleBy);
-          } catch (e) {}
-        });
-      },
-      style: ElevatedButton.styleFrom(
-        foregroundColor: Colors.greenAccent,
-        backgroundColor: PathState.accessiblePath == accessibleBy
-            ? Color(0xff24B9B0)
-            : Colors.white,
-        elevation: 0, // No elevation
-      ),
-      child: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              icon,
-              color: PathState.accessiblePath == accessibleBy
-                  ? Colors.white
-                  : Color(0xff24B9B0),
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 3),
-              child: Text(
-                label,
-                style: TextStyle(
-                  fontFamily: "Roboto",
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                  color: PathState.accessiblePath == accessibleBy
-                      ? Colors.white
-                      : Colors.black,
-                  height: 20 / 14,
+    final bool isSelected = widget.PathState.accessiblePath == widget.accessibleBy;
+
+    return MouseRegion(
+      onEnter: (_) => setState(() => isHovered = true),
+      onExit: (_) => setState(() => isHovered = false),
+      child: GestureDetector(
+        onTap: () {
+          widget.PathState.accessiblePath = widget.accessibleBy;
+          widget.PathState.clearforaccessiblepath();
+          SingletonFunctionController.building.landmarkdata!.then((value) async {
+            try {
+              await Future.delayed(Duration(milliseconds: 50));
+              widget.calculateroute(value.landmarksMap!,
+                  accessibleby: widget.accessibleBy);
+            } catch (e) {}
+          });
+        },
+        child: Container(
+          width: 138,
+          margin: EdgeInsets.only(left: 8),
+          decoration: BoxDecoration(
+            color: isHovered ? Colors.grey[50] : Colors.white,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Stack(
+            children: [
+              // Main content
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      widget.icon,
+                      size: 20,
+                      color: isSelected
+                          ? Color(0xff24B9B0)
+                          : Colors.grey[700],
+                    ),
+                    SizedBox(width: 8),
+                    Text(
+                      widget.label,
+                      style: TextStyle(
+                        fontFamily: "Roboto",
+                        fontSize: 14,
+                        fontWeight: isSelected ? FontWeight.w500 : FontWeight.w400,
+                        color: isSelected
+                            ? Color(0xff24B9B0)
+                            : Colors.grey[900],
+                        height: 20 / 14,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
                 ),
-                textAlign: TextAlign.center,
               ),
-            ),
-          ],
+              // Underline indicator
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
+                  height: 3,
+                  decoration: BoxDecoration(
+                    color: isSelected
+                        ? Color(0xff24B9B0)
+                        : (isHovered ? Color(0xff24B9B0)!.withOpacity(0.3) : Colors.transparent),
+                    borderRadius: BorderRadius.vertical(top: Radius.circular(2)),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

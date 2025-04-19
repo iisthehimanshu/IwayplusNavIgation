3c07b09da380a9a749531dc3c4a4297e61f17d11
Wilson <77532147+Wilson-Daniel@users.noreply.github.com>
2025-04-19 15:46:37 +0530

    Feature - Repository Class

--- lib/NAVIGATION/Repository.dart
+++ lib/NAVIGATION/Repository.dart
@@ -3,11 +3,15 @@ import 'package:iwaymaps/NAVIGATION/APIMODELS/landmark.dart';
 import 'package:iwaymaps/NAVIGATION/DBManager.dart';
 import 'package:iwaymaps/NAVIGATION/Network/APIDetails.dart';
 import 'package:iwaymaps/NAVIGATION/Network/NetworkManager.dart';
+import 'package:iwaymaps/NAVIGATION/waypoint.dart';
 
 import '../IWAYPLUS/API/buildingAllApi.dart';
 import 'API/response.dart';
 import 'APIMODELS/Building.dart';
+import 'APIMODELS/Buildingbyvenue.dart';
+import 'APIMODELS/GlobalAnnotationModel.dart';
 import 'APIMODELS/beaconData.dart';
+import 'APIMODELS/outdoormodel.dart';
 import 'APIMODELS/patchDataModel.dart';
 import 'APIMODELS/polylinedata.dart';
 
@@ -24,7 +28,7 @@ class Repository{
         if(landmarkBox.containsKey(bID)){
             Map<String, dynamic> responseBody = landmarkBox.get(bID)!.responseBody;
             print("Data from DB");
-            return landmarkDetail.conversionFunction!(responseBody);
+            return landmarkDetail.conversionFunction(responseBody);
         }else {
             Response dataFromAPI = await networkManager.request(landmarkDetail);
             print("Data from API");
@@ -33,67 +37,92 @@ class Repository{
     }
 
     Future<polylinedata> getPolylineData(String bID) async {
-        Detail landmarkDetail = apiDetails.landmark(dataBaseManager.getAccessToken(), bID);
-        final landmarkBox = landmarkDetail.dataBaseGetData!();
+        Detail polylineDetail = apiDetails.polyline(dataBaseManager.getAccessToken(), bID);
+        final polylineBox = polylineDetail.dataBaseGetData!();
 
-        if(landmarkBox.containsKey(bID)){
-            Map<String, dynamic> responseBody = landmarkBox.get(bID)!.responseBody;
+        if(polylineBox.containsKey(bID)){
+            Map<String, dynamic> responseBody = polylineBox.get(bID)!.responseBody;
             print("Data from DB");
-            return polylinedata.fromJson(responseBody);
+            return polylineDetail.conversionFunction(responseBody);
         }else {
-            Response dataFromAPI = await networkManager.request(landmarkDetail);
+            Response dataFromAPI = await networkManager.request(polylineDetail);
             print("Data from API");
             return dataFromAPI.data;
         }
     }
 
     Future<patchDataModel> getPatchData(String bID) async {
-        Detail landmarkDetail = apiDetails.landmark(dataBaseManager.getAccessToken(), bID);
-        final landmarkBox = landmarkDetail.dataBaseGetData!();
+        Detail patchDetail = apiDetails.patch(dataBaseManager.getAccessToken(), bID);
+        final patchBox = patchDetail.dataBaseGetData!();
 
-        if(landmarkBox.containsKey(bID)){
-            Map<String, dynamic> responseBody = landmarkBox.get(bID)!.responseBody;
+        if(patchBox.containsKey(bID)){
+            Map<String, dynamic> responseBody = patchBox.get(bID)!.responseBody;
             print("Data from DB");
-            return patchDataModel.fromJson(responseBody);
+            return patchDetail.conversionFunction(responseBody);
         }else {
-            final dataFromAPI = await networkManager.request(landmarkDetail);
+            Response dataFromAPI = await networkManager.request(patchDetail);
             print("Data from API");
-            return patchDataModel.fromJson(dataFromAPI);
+            return dataFromAPI.data;
         }
     }
 
     Future<List<beacon>> getBeaconData(String bID) async {
-        Detail landmarkDetail = apiDetails.landmark(dataBaseManager.getAccessToken(), bID);
-        final landmarkBox = landmarkDetail.dataBaseGetData!();
+        Detail beaconDetail = apiDetails.beacons(dataBaseManager.getAccessToken(), bID);
+        final beaocnBox = beaconDetail.dataBaseGetData!();
 
-        if(landmarkBox.containsKey(bID)){
-            Map<String, dynamic> responseBody = landmarkBox.get(bID)!.responseBody;
+        if(beaocnBox.containsKey(bID)){
+            Map<String, dynamic> responseBody = beaocnBox.get(bID)!.responseBody;
             print("Data from DB");
-            return patchDataModel.fromJson(responseBody);
+            return beaconDetail.conversionFunction(responseBody);
         }else {
-            final dataFromAPI = await networkManager.request(landmarkDetail);
+            Response dataFromAPI = await networkManager.request(beaconDetail);
             print("Data from API");
-            return patchDataModel.fromJson(dataFromAPI);
+            return dataFromAPI.data;
         }
     }
 
-    Future<Building> get(String bID) async {
-        Detail landmarkDetail = apiDetails.landmark(dataBaseManager.getAccessToken(), bID);
-        final landmarkBox = landmarkDetail.dataBaseGetData!();
+    Future<List<Buildingbyvenue>> getBuildingByVenue(String bID) async {
+        Detail buildingByVenueDetail = apiDetails.buildingByVenueApi(dataBaseManager.getAccessToken(), bID);
+        final buildingByVenueBox = buildingByVenueDetail.dataBaseGetData!();
 
-        if(landmarkBox.containsKey(bID)){
-            Map<String, dynamic> responseBody = landmarkBox.get(bID)!.responseBody;
+        if(buildingByVenueBox.containsKey(bID)){
+            Map<String, dynamic> responseBody = buildingByVenueBox.get(bID)!.responseBody;
             print("Data from DB");
-            return Building.fromJson(responseBody);
+            return buildingByVenueDetail.conversionFunction(responseBody);
         }else {
-            final dataFromAPI = await networkManager.request(landmarkDetail);
+            Response dataFromAPI = await networkManager.request(buildingByVenueDetail);
             print("Data from API");
-            return patchDataModel.fromJson(dataFromAPI);
+            return dataFromAPI.data;
         }
     }
 
+    Future<GlobalModel> getGlobalAnnotationData(String bID) async {
+        Detail globaAnnotaionDetail = apiDetails.globalAnnotation(dataBaseManager.getAccessToken(), bID);
+        final globaAnnotaionBox = globaAnnotaionDetail.dataBaseGetData!();
 
+        if(globaAnnotaionBox.containsKey(bID)){
+            Map<String, dynamic> responseBody = globaAnnotaionBox.get(bID)!.responseBody;
+            print("Data from DB");
+            return globaAnnotaionDetail.conversionFunction(responseBody);
+        }else {
+            Response dataFromAPI = await networkManager.request(globaAnnotaionDetail);
+            print("Data from API");
+            return dataFromAPI.data;
+        }
+    }
 
+    Future<List<PathModel>> getWaypointData(String bID) async {
+        Detail waypointDetails = apiDetails.waypoint(dataBaseManager.getAccessToken(), bID);
+        final waypointBox = waypointDetails.dataBaseGetData!();
 
-
+        if(waypointBox.containsKey(bID)){
+            Map<String, dynamic> responseBody = waypointBox.get(bID)!.responseBody;
+            print("Data from DB");
+            return waypointBox.conversionFunction(responseBody);
+        }else {
+            Response dataFromAPI = await networkManager.request(waypointDetails);
+            print("Data from API");
+            return dataFromAPI.data;
+        }
+    }
 }
